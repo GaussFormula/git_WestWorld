1c086941737200772a0d0c525f714e4a4a3d00d4
GaussFormula <c594794@hotmail.com>
2017-12-18 21:35:20 +0800

    StateMachine.h has been done.

--- WestWorld/WestWorld/StateMachine.h
+++ WestWorld/WestWorld/StateMachine.h
@@ -6,6 +6,7 @@
 //	Desc: State machine class. Inherit from this class and create
 //		  some state to give your agents FSM functionality
 #include <string>
+#include <iostream>
 
 #include "State.h"
 #include "Telegram.h"
@@ -25,6 +26,98 @@ public:
 	void SetCurrentState(State<entity_type>* s) { m_pCurrentState = s; }
 	void SetPreviousState(State<entity_type>* s) { m_pPreviousState = s; }
 	void SetGlobalState(State<entity_type>* s) { m_pGlobalState = s; }
+
+	//call this to update the FSM
+	void Update()const
+	{
+		//if a global state exists,call its execute method, else do nothing
+		if (m_pGlobalState)
+		{
+			m_pGlobalState->Execute(m_pOwner);
+		}
+
+		//same for the current state
+		if (m_pCurrentState)
+		{
+			m_pCurrentState->Execute(m_pOwner);
+		}
+	}
+
+	bool HandleMessage(const Telegram& msg)const
+	{
+		//first see if the current state is valid and that it can handle
+		//the message
+		if (m_pCurrentState&&m_pCurrentState->OnMessage(m_pOwner, msg))
+		{
+			return true;
+		}
+		//if not, and if a global state has been implemented, send the message
+		//to the global state
+		if (m_pGlobalState&&m_pGlobalState->OnMessage(m_pOwner, msg))
+		{
+			return true
+		}
+		return false;
+	}
+
+	//change to a new state
+	void ChangeState(State<entity_type>* pNewState)
+	{
+		if (pNewState)
+		{
+			std::cout << "<StateMachine::ChangeState>:trying to assign null state to current" 
+					  << std::endl;
+
+			//keep a record of the previous state
+			m_pPreviousState = m_pCurrentState;
+
+			//call the exit method of the existing state
+			m_pCurrentState->Exit(m_pOwner);
+
+			//change state to the new state
+			m_pCurrentState = pNewState;
+
+			//call the entry method of the new state
+			m_pCurrentState->Enter(m_pOwner);
+		}
+	}
+
+	//change state back to the previous state
+	void RevertToPreviousState()
+	{
+		ChangeState(m_pPreviousState);
+	}
+
+	//returns true if the current state's type is equal to the type of the
+	//class passed as a parameter
+	bool isInState(const State<entity_type>& st)const
+	{
+		if (typeid(*m_pCurrentState) == typeid(st))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	State<entity_type>* GetCurrentState()const { return m_pCurrentState; }
+	State<entity_type>* GetGlobalState()const { return m_pGlobalState; }
+	State<entity_type>* GetPreviousState()const { return m_pPreviousState; }
+
+	//only ever used during debugging to grab the name of the current state
+	std::string GetNameOfCurrentState()const
+	{
+		std::string s(typeid(*m_pCurrentState).name());
+
+		//remove the 'class ' part from the front of the string
+		if (s.size() > 5)
+		{
+			s.erase(0, 6);
+		}
+		return s;
+	}
 
 private:
 	//a pointer to the agent that owns this instance
